bf3477ee159e6b6627e442288f0b2bbe8a0fef4d
Ernesto Prevost <nestoki9708@gmail.com>
2025-05-19 12:59:19 -0400

    ajustes del mrp

--- mrp_joyca/models/models.py
+++ mrp_joyca/models/models.py
@@ -3,26 +3,26 @@ from odoo import models, fields
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    # Redefinir el campo 'state' con los nuevos estados
     state = fields.Selection(
         selection_add=[
-            ('draft', 'Borrador'),
             ('medicion', 'Medición'),
             ('sketchup', 'Sketch Up'),
             ('layout', 'Layout'),
             ('fabricacion', 'Fabricación'),
             ('barnizado', 'Barnizado'),
             ('montaje', 'Montaje'),
-            ('done', 'Finalizado'),
         ],
-        default='draft',
         ondelete={
-            'draft': 'set default',
             'medicion': 'set default',
             'sketchup': 'set default',
             'layout': 'set default',
             'fabricacion': 'set default',
             'barnizado': 'set default',
             'montaje': 'set default',
-        }
+        },
+        # Elimina compute y readonly para permitir cambios manuales:
+        compute=None,
+        readonly=False,
+        default='draft',
+        tracking=True,  # Opcional: para seguimiento en chatter
     )
