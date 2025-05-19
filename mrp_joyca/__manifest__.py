753f372f86aec2ec87c7343ecaf3842fae0061e8
Ernesto Prevost <nestoki9708@gmail.com>
2025-05-19 12:18:12 -0400

    ajustes del modulo de mrp

--- mrp_joyca/__manifest__.py
+++ mrp_joyca/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sale_mrp_automatizaciones",
+    'name': "mrp_joyca_state,
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
@@ -15,20 +15,20 @@ Long description of module's purpose
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '17.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'mrp'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        'views/mrp_production_views.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'auto_install': False,
+    'installable': True,
+    'license': 'LGPL-3',
 }
 
---
+++ mrp_joyca/models/models.py
@@ -0,0 +1,27 @@
+from odoo import models, fields
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    # Redefinir el campo 'state' con los nuevos estados
+    state = fields.Selection(
+        selection_add=[
+            ('draft', 'Borrador'),
+            ('medicion', 'Medición'),
+            ('sketchup', 'Sketch Up'),
+            ('layout', 'Layout'),
+            ('fabricacion', 'Fabricación'),
+            ('barnizado', 'Barnizado'),
+            ('montaje', 'Montaje'),
+            ('done', 'Finalizado'),
+        ],
+        ondelete={
+            'draft': 'set default',
+            'medicion': 'set default',
+            'sketchup': 'set default',
+            'layout': 'set default',
+            'fabricacion': 'set default',
+            'barnizado': 'set default',
+            'montaje': 'set default',
+        }
+    )
