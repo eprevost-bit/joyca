78773e432d9f6614c0736c20cf75a8d244066963
Ernesto Prevost <nestoki9708@gmail.com>
2025-07-03 09:21:19 -0400

    ajustees de las horas trabajadas

--- ibec_portal_empleado/models/models.py
+++ ibec_portal_empleado/models/models.py
@@ -1,7 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
 
 class HrAttendance(models.Model):
     _inherit = 'hr.attendance'
 
     archived = fields.Boolean(string="Archivado", default=False)
     auto_generated = fields.Boolean(string="Generado Automáticamente", default=False)
+
+    x_worked_time_calculated = fields.Float(
+        string="Tiempo Calculado",
+        compute='_compute_worked_time_calculated',
+        store=True,  # Opcional: para poder buscar y agrupar por este campo
+        help="Calcula las horas trabajadas directamente de la entrada y salida, sin otras consideraciones."
+    )
+
+    @api.depends('check_in', 'check_out')
+    def _compute_worked_time_calculated(self):
+        for attendance in self:
+            if attendance.check_out and attendance.check_in:
+                delta = attendance.check_out - attendance.check_in
+                attendance.x_worked_time_calculated = delta.total_seconds() / 3600.0
+            else:
+                attendance.x_worked_time_calculated = 0.0
