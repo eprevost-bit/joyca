7287396bd4aa3c9a332e2bf0ed925594930fcabb
Ernesto Prevost <nestoki9708@gmail.com>
2025-07-29 05:44:00 -0400

    ajustes de las ventas joyca

--- sale_joyca/models/models.py
+++ sale_joyca/models/models.py
@@ -15,13 +15,13 @@ class SaleOrder(models.Model):
     )
 
     def action_create_new_version(self):
-        self.ensure_one() # Aseguramos que solo se ejecuta para un registro
+        self.ensure_one()  # Aseguramos que solo se ejecuta para un registro
 
         # Usamos expresiones regulares para encontrar el nombre base y la versión
         match = re.match(r'^(.*?)(?:-V(\d+))?$', self.name)
         base_name, version_number_str = match.groups()
         current_version = int(version_number_str) if version_number_str else 0
-        
+
         new_version_number = 0
 
         # --- Lógica para manejar el presupuesto ORIGINAL ---
@@ -36,23 +36,18 @@ class SaleOrder(models.Model):
             new_version_number = 2
         else:
             # Si ya es una versión (ej. S00040-V1), solo cambiamos su estado a 'Versión'.
-            # No es necesario cambiarle el nombre.
             self.write({
                 'state': 'version'
             })
             # La nueva versión será la siguiente a la actual
             new_version_number = current_version + 1
 
-        # Forzamos que los cambios se guarden en la base de datos antes de continuar.
-        # Esto evita problemas de concurrencia o de que el estado no se guarde a tiempo.
         self.flush_model(['name', 'state'])
-
-        # --- Lógica para crear el NUEVO presupuesto ---
         new_version_name = f"{base_name}-V{new_version_number}"
         new_quotation = self.copy(default={
             'name': new_version_name,
-            'state': 'version', # El nuevo presupuesto empieza en borrador
-            'origin': self.name, # Guardamos como origen el nombre del presupuesto del que venimos
+            'state': 'draft',  # El nuevo presupuesto empieza como un borrador activo.
+            'origin': self.name,  # Guardamos como origen el nombre del presupuesto del que venimos
         })
 
         # Devolvemos una acción para que Odoo abra el nuevo presupuesto en pantalla
