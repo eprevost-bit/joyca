ef95d82d5a2cfbd1fca01b38e2e0651aca723e32
Nestoki-work <nestoki9708@gmail.com>
2025-10-08 03:03:31 +0200

    ajustes generales de las ultimas funcionalidades

--- coton_purchase_env/models/purchase_order.py
+++ coton_purchase_env/models/purchase_order.py
@@ -7,7 +7,7 @@ class PurchaseOrderLineCustom(models.Model):
 
     # 1. Campo para la casilla de selección en cada línea
     is_selected_for_email = fields.Boolean(string="Seleccionar para Correo")
-    proveedor_line = fields.Many2one('res.partner', string='Partner')
+    proveedor_line = fields.Many2one('res.partner', string='Proveedor')
 
 
 class PurchaseOrderCustom(models.Model):
